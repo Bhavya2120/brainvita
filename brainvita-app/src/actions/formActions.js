0ff42a22796a3a4d989590571194138cc151eff9
Bhavya Dua <78466683+Bhavya2120@users.noreply.github.com>
2024-05-08 16:34:49 +0530

    Update formActions.js

--- brainvita-app/src/actions/formActions.js
+++ brainvita-app/src/actions/formActions.js
@@ -1,16 +1,24 @@
 import { SUBMIT_FORM } from '../const/actionTypes';
+import axios from 'axios';
 
 /**
  * @description action for submit user with left marbles
  */
 export const submitForm = (userForm) => {
+    const headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    };
+
     return {
         type: SUBMIT_FORM,
-        payload: new Promise((resolve, reject) => {
-            setTimeout(() => {
-                alert('Storing score is a work in progress :)')
-                resolve();
-            }, 1000);
-        })
+        payload: axios.post('http://127.0.0.1:8000/form/', userForm, { headers })
+            .then(response => {
+                alert('Form data submitted successfully!');
+                return response.data;
+            })
+            .catch(error => {
+                alert('Error submitting form data');
+                throw error;
+            })
     };
 }
